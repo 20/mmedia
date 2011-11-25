3de5da73268b3d6444002dcb24f2e58a4acec792
admin <admin@mocambos.net>
2011-11-25 18:38:17 +0100

    Iniciando o codigo para serializaçao

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from django.core import serializers
 
 import os
 
@@ -12,6 +13,9 @@ def _path(instance):
 def _path_to_upload(instance, filename):
     # Come faccio a scegliere il repository/directory dove salvare? Per ora da settings
     return os.path.join(settings.GITANNEX_DIR, settings.PORTAL_NAME, instance.author.username, instance.mediatype, filename)
+
+def createObjectFromFiles():
+    pass
 
 
 class MMedia(models.Model):
@@ -28,10 +32,11 @@ class MMedia(models.Model):
 #    tags = TagField(verbose_name=_('tags'), help_text=tagfield_help_text)
 
     def path(self):
-        return _path(self)
+        return os.path.join(settings.MEDIA_ROOT, settings.GITANNEX_DIR, settings.PORTAL_NAME, self.author.username, self.mediatype, \
+                                self.fileref.path)
 
     def path_relative(self):
-        return os.path.join(self.author.username, self.mediatype, \
+        return os.path.join(settings.GITANNEX_DIR, settings.PORTAL_NAME, self.author.username, self.mediatype, \
                                 self.fileref.path)
 
     def __unicode__(self):
@@ -40,6 +45,17 @@ class MMedia(models.Model):
     class Meta:
         abstract = True
 
+    def save(self, *args, **kwargs):
+        print type(self)
+        serializeTo = os.path.join(settings.MEDIA_ROOT, settings.GITANNEX_DIR,\
+                                       settings.PORTAL_NAME, settings.SERIALIZED_DIR,\
+                                       os.path.basename(self.fileref.path) + '.xml')
+        print '>>>> Serialize to: ' + serializeTo
+        out = open(serializeTo, "w")
+        XMLSerializer = serializers.get_serializer("xml")
+        xml_serializer = XMLSerializer()
+        xml_serializer.serialize((self, ), stream=out)
+        super(MMedia, self).save(*args, **kwargs)
         
 class Audio(MMedia):
     mediatype = "audio"
@@ -68,6 +84,4 @@ class Video(MMedia):
         verbose_name_plural = _('videos')
 
 
-def createObjectFromFiles():
-    pass
 
--- signals.py
+++ signals.py
@@ -2,14 +2,28 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 from gitannex.signals import filesync_done
-#from gitannex.models import GitAnnexRepository
 
 from gitannex.models import GitAnnexRepository
 from mmedia.models import createObjectFromFiles
 
+from django.core import serializers
+
+from django.conf import settings
+
 # Listen to signals /aka plugin to gitannex
 
-@receiver(filesync_done, sender=GitAnnexRepository)
-def syncGitAnnexRepository(sender, **kwargs):
+@receiver(filesync_done, instance=GitAnnexRepository)
+def syncGitAnnexRepository(instance, **kwargs):
     # Fai il sync.. ri
-    createObjectFromFiles()
+    for root, dirs, files in os.walk(\
+        os.path.join(settings.MEDIA_ROOT, settings.GITANNEX_DIR, instance.repositoryURLOrPath)):
+        for file in files:
+            if file.endswith('.xml'):
+                xmlIn = open(file, "r")
+                XMLSerializer = serializers.get_serializer("xml")
+                xml_serializer = XMLSerializer()
+                for obj in xml_serializer.deserialize("xml", xmlIn):
+                    obj.id = None
+                    obj.pk = None
+                    obj.save()
+                os.remove(file)
